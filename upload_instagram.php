935f0eb3b84834d1806901847a4d079f5fc37894
bio-boris <sadkhin2@sadkhin2.local>
2016-12-27 11:00:29 -0600

    changes

--- upload_instagram.php
+++ upload_instagram.php
@@ -6,14 +6,11 @@ error_reporting(E_ALL);
 require("vendor/autoload.php");
 $instagram = new \InstagramAPI\Instagram();
 
-
-
-
 /////// CONFIG ///////
 $username = 'bsadkhin2';
 $password = 'password123!';
 $log = "log.txt";
-$debug = true;
+$debug = false;
 $base_dir = 'images';
 $image_dir = "$base_dir/todo";
 $completed_dir="$base_dir/done";
@@ -35,38 +32,52 @@ foreach(new RecursiveIteratorIterator($it) as $file)
 $count = count($files);
 $number_of_files_to_upload = $number_of_files_to_upload <= $count ? $number_of_files_to_upload : $count;
 
-///////
+///////LOGIN
 
-for($i = 1 ; $i< $number_of_files_to_upload; $i++){
-    shuffle($files);
-    $photo = array_pop($files); 
-    print "About to upload $photo ( $i of $number_of_files_to_upload) " . date(DATE_RFC2822) . "\n";
+$msg = "About to LOGIN to instagram"; 
+fwrite(STDERR, "$msg\n");
 
-    sleep(60);
-
-    $i = new \InstagramAPI\Instagram($debug);
-    $i->setUser($username, $password);
-    try {
-        $i->login(true);
-    } catch (Exception $e) {
-        $e->getMessage();
-        print "Couldn't login";
-        exit();
+$instagram = new \InstagramAPI\Instagram($debug);
+$instagram->setUser($username, $password);
+try {
+    $instagram->login(true);
+} catch (Exception $e) {
+    $e->getMessage();
+    print "Couldn't login";
+    exit();
     }
-    try {
+
+///UPLOAD    
+for($i = 1 ; $i<= $number_of_files_to_upload; $i++){
+
+    shuffle($files);
+    $photo = array_pop($files);
+
+    //Not Logged
+    $msg = "About to upload $photo"; 
+    fwrite(STDERR, "$msg\n");
+
+#Logging and renaming
+  try {
         $caption = "";
-        $i->uploadPhoto($photo, $caption);
+        $instagram->uploadPhoto($photo, $caption);
         $success=1;
     } catch (Exception $e) {
         $success=0;
-        echo $e->getMessage();
-        print "UPLOAD FAIL " . $photo;
+        print $e->getMessage();
+        print "Failed to upload $photo\t" . date(DATE_RFC2822) . "\n";
     }
     if($success==1){
         $fn = basename($photo);
         rename($photo,"$completed_dir/$fn");
+        print "Successfully uploaded $photo\t" . date(DATE_RFC2822) . "\n"; 
+        
+        //Not logged 
+        $msg = "About to upload $photo"; 
+        fwrite(STDERR, "$msg\n"); 
+        sleep(10);
     }
 }
 
-print "\t " . date(DATE_RFC2822) . "\n";
+fclose(handle)write(STDERR, "\n\n");
 ?>
